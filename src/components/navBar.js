e80117df0e8259b1ec23aac7615dc55d77186dd7
Santiago <santipicunino@gmail.com>
2022-03-21 00:04:00 -0300

    Commit clase 2

---
+++ src/components/navBar.js
@@ -0,0 +1,25 @@
+
+
+
+
+function navBar(){
+    return(
+        
+          <header className='main-header'>
+            <div className='container-logo'>
+            <img src="logo192.png" className="img-header"  />
+            </div>
+    
+            <ul className='navBar'>
+            <li><button>Home</button></li>
+            <li><button>Productos</button></li>
+            <li><button>Nosotros</button></li>
+            <li><button>Contacto</button></li>
+            </ul>
+    
+          </header>
+          
+      )
+}
+
+export default navBar
